e36f03effe282c67c279e389a8351da53fee2a0b
Vlad <dr.melix@gmail.com>
2024-07-20 23:17:27 +0300

    Добавил подключение к PostgreSQL с использованием dotenv для загрузки конфигурационных параметров

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-DB_DSN =...
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+DB_DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
